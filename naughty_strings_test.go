2997b057c62441d05dc984d444e1c769b43cfce2
justing-bq <62349012+justing-bq@users.noreply.github.com>
2020-07-31 13:58:05 -0700

    Rename NewReaderStr() to NewReaderString() (#52)

--- naughty_strings_test.go
+++ naughty_strings_test.go
@@ -105,9 +105,9 @@ func NaughtyStrings(t *testing.T, tv testValue, s string) {
 	ionHashWriter, ok := hw.(*hashWriter)
 	require.True(t, ok, "Expected hw to be of type hashWriter")
 
-	writeFromReaderToWriter(t, ion.NewReaderStr(s), ionHashWriter)
+	writeFromReaderToWriter(t, ion.NewReaderString(s), ionHashWriter)
 
-	hr, err := NewHashReader(ion.NewReaderStr(s), hasherProvider)
+	hr, err := NewHashReader(ion.NewReaderString(s), hasherProvider)
 	require.NoError(t, err, "Expected NewHashReader() to successfully create a HashReader")
 
 	ionHashReader, ok := hr.(*hashReader)
